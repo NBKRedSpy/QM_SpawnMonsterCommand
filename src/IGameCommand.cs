6643bab7881cdfd6d24e35ec42be838f32e1a265
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2025-10-05 20:42:06 -0400

    Multi version support

---
+++ src/IGameCommand.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace SpawnMonsterCommand_Bootstrap
+{
+    /// <summary>
+    /// The contract use to relay a game command to a version
+    /// </summary>
+    public interface IGameCommand
+    {
+        string Execute(string[] tokens);
+        List<string> FetchAutocompleteOptions(string command, string[] tokens);
+        string Help(string command, bool verbose);
+        bool IsAvailable();
+        bool ShowInHelpAndAutocomplete();
+    }
+}
---
+++ src/SpawnMonsterCommandRelay.cs
@@ -0,0 +1,45 @@
+﻿using MGSC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpawnMonsterCommand_Bootstrap
+{
+    /// <summary>
+    /// Relays the SpawnMonsterCommand to the beta or stable version via reflection.
+    /// Required since the game uses static functions via convention.
+    /// </summary>
+    [ConsoleCommand(new string[] { CommandName, "smuc" })]
+    public class SpawnMonsterCommandRelay 
+    {
+        public const string CommandName = "spawn-monster-under-cursor";
+
+        private static IGameCommand Command;
+
+        public static void Init(Assembly assembly, string typeName)
+        {
+            Type type = assembly.GetType(typeName, true);
+
+            Command = Activator.CreateInstance(type) as IGameCommand;
+
+            if (Command == null)
+            {
+                throw new Exception($"Failed to create instance of type {typeName} as IGameCommand");
+            }   
+        }
+
+        public static string Help(string command, bool verbose) => Command.Help(command, verbose);
+
+        public string Execute(string[] tokens) => Command.Execute(tokens);
+
+        public static List<string> FetchAutocompleteOptions(string command, string[] tokens) => 
+            Command.FetchAutocompleteOptions(command, tokens);
+
+        public static bool IsAvailable() => Command.IsAvailable();
+
+        public static bool ShowInHelpAndAutocomplete() => Command.ShowInHelpAndAutocomplete();
+    }
+}
